f9229295366312c7206710c38230b897d53fa8c8
Stephen31 <stephen.baroan@gmail.com>
2018-02-03 00:06:31 +0100

    [Feat] Finishint impl of client

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './styles/style.css';
 import UserListContainer from './containers/UserListContainer';
 import TodoListContainer from './containers/TodoListContainer';
 import { Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return 
-      <Switch>
+const App = () => {
+    return <Switch>
         <Route exact path='/' component={UserListContainer}/>
         <Route path='/todos/:userId' component={TodoListContainer}/>
-      </Switch>
-    // return (
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <h1 className="App-title">Welcome to React</h1>
-    //     </header>
-    //     <p className="App-intro">
-    //       To get started, edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //   </div>
-    // );
-  }
-}
+    </Switch>
+};
 
 export default App;
